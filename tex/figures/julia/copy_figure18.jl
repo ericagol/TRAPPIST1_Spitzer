9a54dfb3361633a35f3cbf6a5fd01655e78cef20
Eric Agol <agol@uw.edu>
2020-09-24 14:04:26 -0700

    Updating Figure 18; adding definistion of \Delta IW

--- tex/figures/julia/copy_figure18.jl
+++ tex/figures/julia/copy_figure18.jl
@@ -1,5 +1,5 @@
 
 # Copies figure 18 to figures directory:
 
-#run(`cp ../static/figure_water_period_TTV_trim.png  ..`)
-run(`cp ../static/figure_water_content_versus_period_agol_et_al.png  ../figure_water_period_TTV_trim.png`)
+run(`cp ../static/figure_water_period_TTV_trim.png  ..`)
+#run(`cp ../static/figure_water_content_versus_period_agol_et_al.png  ../figure_water_period_TTV_trim.png`)
